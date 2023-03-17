bb5787526e8d33917525b57ee16f1f94c78a3719
YINGMEI <j.yeongmee@gmail.com>
2023-03-17 23:26:14 +0900

    회원가입 로그인 기능 구현 완료

--- Hangligarli/src/main/java/com/sparta/hangligarli/exception/ResponseMessage.java
+++ Hangligarli/src/main/java/com/sparta/hangligarli/exception/ResponseMessage.java
@@ -1,4 +1,41 @@
 package com.sparta.hangligarli.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class ResponseMessage {
+    private final String message;
+    private final int statusCode;
+    private final Object data;
+
+    public static ResponseEntity ErrorResponse(CustomErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ResponseMessage.builder()
+                        .statusCode(errorCode.getHttpStatus().value())
+                        .message(errorCode.getMessage())
+                        .data("")
+                        .build()
+                );
+    }
+
+    public static ResponseEntity SuccessResponse(String message, Object data) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ResponseMessage.builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .message(message)
+                        .data(data)
+                        .build()
+                );
+    }
 }
+
--- Hangligarli/src/main/java/com/sparta/hangligarli/service/UserService.java
+++ Hangligarli/src/main/java/com/sparta/hangligarli/service/UserService.java
@@ -1,4 +1,62 @@
 package com.sparta.hangligarli.service;
 
+import com.sparta.hangligarli.dto.LoginRequestDto;
+import com.sparta.hangligarli.dto.SignupRequestDto;
+import com.sparta.hangligarli.entity.Post;
+import com.sparta.hangligarli.entity.User;
+import com.sparta.hangligarli.entity.UserRoleEnum;
+import com.sparta.hangligarli.exception.CustomErrorCode;
+import com.sparta.hangligarli.exception.CustomException;
+import com.sparta.hangligarli.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+    @Transactional
+    public String signup(SignupRequestDto signupRequestDto) {
+        // 회원아이디 중복 확인
+        boolean found = userRepository.findByUsername(signupRequestDto.getUsername()).isPresent();
+        if (found) {
+            throw new CustomException(CustomErrorCode.DUPLICATE_USER);
+        }
+
+        //닉네임 중복 확인
+        found = userRepository.findByNickname(signupRequestDto.getNickname()).isPresent();
+        if (found) {
+            throw new CustomException(CustomErrorCode.DUPLICATE_NICKNAME);
+        }
+
+        User user = new User(signupRequestDto, passwordEncoder.encode(signupRequestDto.getPassword()));
+        userRepository.save(user);
+
+        return "회원가입 성공";
+    }
+
+    @Transactional(readOnly = true)
+    public Map<String, String> login(LoginRequestDto loginRequestDto) {
+        // 사용자 확인
+        User user = userRepository.findByUsername(loginRequestDto.getUsername()).orElseThrow(
+                () -> new CustomException(CustomErrorCode.USER_NOT_FOUND)
+        );
+        // 비밀번호 확인
+        if(!passwordEncoder.matches(loginRequestDto.getPassword(), user.getPassword())){
+            throw  new CustomException(CustomErrorCode.NOT_PROPER_PASSWORD);
+        }
+
+        Map<String, String> result = new HashMap<>();
+        result.put("username", user.getUsername());
+        result.put("nickname", user.getNickname());
+
+        return result;
+    }
 }
